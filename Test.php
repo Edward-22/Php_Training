6cb3c02a9e35c0af286ed134ba0d9496ebb7300a
Edward-22 <NuwL1fe@GSS!>
2022-11-22 12:37:09 +0200

    Foundation_Task_Php

--- Test.php
+++ Test.php
@@ -1,2 +1,11 @@
 <?php
-echo "FT5";
+class Fibonacci{
+    public static function getSequence($TotalIterationsInt){
+        $SequenceArr = [0,1];
+        for ($Int = 0; $Int <$TotalIterationsInt; $Int++) {
+            $SequenceArr[$Int+2] = $SequenceArr[$Int] + $SequenceArr[$Int+1];
+        }
+        return $SequenceArr;
+    }
+}
+echo json_encode(Fibonacci::getSequence(8));
