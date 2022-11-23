c36259f67ed70239a200b94d6806cbf772dc20ef
Edward-22 <NuwL1fe@GSS!>
2022-11-23 11:02:37 +0200

    Foundation_Task_Php

--- Test.php
+++ Test.php
@@ -1,23 +1,2 @@
 <?php
-// as loop
-function addAll($Array) {                           // function implementation
-    $TotalInt = 0;                                  // variable stated
-    foreach ($Array as $Int) {                      // loops same amount of times as elements in $Array
-        $TotalInt += array_sum($Array);             // Numbers in arrays are added up and set to variable $TotalInt
-        array_shift($Array);                        // Removes first element of array
-    }
-    return $TotalInt;                               // Function returns a value
-}
-echo addAll([1,1,1,1,1]);                           // used to display and call the function addAll()
-
-// as recursion function
-function addAllRecursion($Array, &$SumInt) {                // Function with array and reference int
-    if (count($Array) > 0) {                        // Elements counted and checked if greater than 0
-        $SumInt += array_sum($Array);               // Numbers in arrays are added up and set to $SumInt
-        array_shift($Array);                        // Removes first element of array
-        addAllRecursion($Array, $SumInt);                   // Recurs the function
-    }
-}
-$SumInt = 0;
-addAllRecursion([1,1,1,1,1],$SumInt);                       // Assigns values to $Array
-echo $SumInt."<br>";                                // Displays sum total after execution of other code
+echo "RU";
