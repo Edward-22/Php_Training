fa5f6dd3c17a82b27e5aec5e4f6e99b196feb56f
Edward-22 <NuwL1fe@GSS!>
2022-11-24 13:54:38 +0200

    Foundation_Task_Php

--- Test.php
+++ Test.php
@@ -1,2 +1,31 @@
 <?php
-echo "RU";
+$servername = "localhost";
+$username = "phpmyadmin";
+$password = "password";
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+/*$sql = "CREATE DATABASE Person";
+if($conn->query($sql)===TRUE) {
+    echo "Database created successfully";
+}
+else {
+    echo "Error creating database: ".$conn->error;
+}*/
+$sql = "CREATE TABLE Person (
+Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+FirstName VARCHAR(30) NOT NULL,
+Surname VARCHAR(30) NOT NULL,
+EmailAddress VARCHAR(50),
+DateOfBirth DATE NOT NULL
+)";
+$sql = "USE Person";
+if ($conn->query($sql)===TRUE) {
+    echo "Table Person created successfully";
+}
+else {
+    echo "Error creating table: ".$conn->error;
+}
+$conn->close();
