6cbea9aed16173478b45cc5feaafe499d7ecc113
Edward-22 <NuwL1fe@GSS!>
2022-12-14 15:30:04 +0200

    Foundation_Task_Php

--- Test.php
+++ Test.php
@@ -1,2 +1,11 @@
 <?php
-//GG
+function addAll($Array) {
+    $CountInt = 0;
+    foreach($Array as $Int) {
+        $TotalInt = array_sum($Array);
+        $CountInt += $TotalInt;
+        array_pop($Array);
+    }
+    return "5+4+3+2+1=".$CountInt;
+}
+echo addAll([1,1,1,1,1]);
