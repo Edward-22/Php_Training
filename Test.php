9851b2891b6ea8501d504beaea5824c34eaea482
Edward-22 <NuwL1fe@GSS!>
2022-12-15 12:02:18 +0200

    Foundation_Task_Php

--- Test.php
+++ Test.php
@@ -1,10 +1,25 @@
 <?php
-$MaxNumberInt = $_POST["Maximum_Number"];
-$IterationCountInt = 0;
+
+/*$IterationCountInt = 0;
 $SequenceArr = [0,1];
 do {
     $SequenceArr[$IterationCountInt + 2] = $SequenceArr[$IterationCountInt] + $SequenceArr[$IterationCountInt + 1];
     $IterationCountInt++;
 }
 while ($MaxNumberInt != end($SequenceArr));
-echo json_encode($SequenceArr);
+echo json_encode($SequenceArr);*/
+
+$InitialNumbersArr = [0,1];
+$FibonacciArr = [0,1];
+$MaximumNumberInt = $_POST["Maximum_Number"];
+if ($MaximumNumberInt<=0) {
+    alert("Only give numers from 1 and up!");
+}
+echo "Fibonacci is cool!<br>";
+for ($Int = 0; $Int <= $MaximumNumberInt; $Int = array_sum($InitialNumbersArr)) {
+    $NextInt = array_sum($InitialNumbersArr);
+    array_shift($InitialNumbersArr);
+    array_push($InitialNumbersArr, $NextInt);
+    array_push($FibonacciArr, $NextInt);
+}
+echo json_encode($FibonacciArr);
