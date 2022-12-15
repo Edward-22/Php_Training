5fdf510c0ee71e42988ed2c1dad0ad7232e0dfdf
Edward-22 <NuwL1fe@GSS!>
2022-12-15 11:36:39 +0200

    Foundation_Task_Php

--- Test.php
+++ Test.php
@@ -4,6 +4,7 @@ class Palindrome {
     public static function isPalindrome($WordStr) {
         $WordStr = str_replace(' ', '', strtolower($WordStr));
         $TempStr = strrev($WordStr);
+        $TempStr = preg_replace('/[^A-Za-z0-9\-]', '',$TempStr);
         if ($TempStr == $WordStr) {
             return true;
         } else {
@@ -11,9 +12,8 @@ class Palindrome {
         }
     }
 }
-
 if (Palindrome::isPalindrome("race Car")) {
-    echo "Is a Palindrome";
+    echo "IS a Palindrome!";
 } else {
-    echo "Not a forking palindrome m8!";
+    echo "NOT a forking palindrome mate.";
 }
